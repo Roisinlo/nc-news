455c4c92dfc31f18fa079ef4cd0b0c5e177e8ab1
Roisinlo <roisinclarelong@gmail.com>
2023-04-18 14:17:07 +0100

    links from article list to individual article added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
-import Header from './components/Header';
-import { Route, Routes } from 'react-router-dom';
-import ArticlesList from './components/ArticlesList'
-import NavBar from './components/NavBar';
-import ArticleById from './components/ArticleById';
+import "./App.css";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import ArticlesList from "./components/ArticlesList";
+import NavBar from "./components/NavBar";
+import ArticleById from "./components/ArticleById";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <NavBar/>
+      <NavBar />
       <Routes>
-      <Route path='/' element={<ArticlesList/>}/>
-      <Route path='/api/articles/:article_id' element={<ArticleById/>}/>
+        <Route path="/" element={<ArticlesList />} />
+        <Route path="/articles" element={<ArticlesList />} />
+        <Route path="/articles/:article_id" element={<ArticleById />} />
       </Routes>
     </div>
   );
--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,12 +1,15 @@
-function ArticleCard (article){
+import { Link } from "react-router-dom"
+
+function ArticleCard ({article}){
+
     return(
         <section>
-            <h4>{article.article.title}</h4>
-            <h5>By {article.article.author}</h5>
-            <img src={article.article.article_img_url} alt={article.article.title} width='300' height-='auto'></img>
-            <h5>Topic: {article.article.topic}</h5>
-            <p>Comments: {article.article.comment_count}</p>
-            <p>Votes: {article.article.votes}</p>
+            <h4><Link to={`/articles/${article.article_id}`}>{article.title}</Link></h4>
+            <h5>By {article.author}</h5>
+            <img src={article.article_img_url} alt={article.title} width='300' height-='auto'></img>
+            <h5>Topic: {article.topic}</h5>
+            <p>Comments: {article.comment_count}</p>
+            <p>Votes: {article.votes}</p>
         </section>
     )
 };
