75fd752784815a16e8c9eedc11f7d1f4cf677ac7
Roisinlo <roisinclarelong@gmail.com>
2023-05-16 12:49:59 +0100

    topic

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,13 +1,19 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
-function ArticleCard ({article}){
+function ArticleCard ({article, topic}){
+    let [searchParams, setSearchParams] = useSearchParams();
+
+    const handleTopicClick= (eve) => {
+
+    }
 
     return(
         <section className="allArticle">
             <h4><Link to={`/articles/${article.article_id}`}>{article.title}</Link></h4>
             <h5>By {article.author}</h5>
             <img src={article.article_img_url} alt={article.title} width='300' height-='auto'></img>
-            <p><Link to={`/articles/${article.topic}`}>Topic: {article.topic}</Link></p>
+            <p>Topic: {article.topic}</p>
             <p>Comments: {article.comment_count}</p>
             <p>Votes: {article.votes}</p>
         </section>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,26 @@
-// import { useState } from 'react'
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-function NavBar(){
-// const [topics, setTopics] = useState([]);
-// const [isLoading, setIsLoading] = useState(true)
+function NavBar({ topics }) {
+  return (
+    <section className="navbar">
+      <button className="buttons">
+        <Link to={"/"}>Home</Link>
+      </button>
+      <button className="buttons">Users/login</button>
 
-
-    return(
-        <section className="navbar">
-            <button className="buttons"><Link to={"/"}>Home</Link></button>
-            <button className="buttons">Users/login</button>
-        </section>
-    )
-};
+      <div className="navbar">
+        <Link key={"All"} to={"/articles"}>
+          <button className="buttons">All Topics</button>
+        </Link>
+        {topics?.map((topic) => {
+          return (
+            <Link key={topic.slug} to={`/articles?topic=${topic.slug}`}>
+              <button className="buttons">{topic.slug}</button>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
 export default NavBar;
